a218f9be6a624c5c4939ece2404d9771f068b3f8
Benjamin <benjamin@codespace.co.za>
2023-12-13 20:13:24 +0200

    React router setup continue

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -3,18 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './components/home/home'
-import { Route, Routes } from 'react-router-dom'
+import ShopPage from './components/shop/shopPage'
+import ShoppingCart from './components/shop/shoppingCart'
+import ContactPage from './components/contact/contactPage'
+import { Link, Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
 
   return (
     <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/shop">Shop</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/contact">Contact Us</Link></li>
+      
+      </ul>
+    </nav>
 <Routes>
-  <Route path="/" element= {<Home />} />
-  <Route path="/shop"/>
-  <Route path="/cart"/>
-  <Route path="/contact"/>
+  <Route path="/" element={<Home />} />
+  <Route path="/shop" element={<ShopPage /> } />
+  <Route path="/cart" element={<ShoppingCart />} />
+  <Route path="/contact" element={<ContactPage />} />
 </Routes>
     </>
   )
